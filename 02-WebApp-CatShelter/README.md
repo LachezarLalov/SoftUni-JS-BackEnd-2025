4d4540b7012d8829386a0a19d42214ad0785180c
Lachezar Lalov <lacho.lalov@gmail.com>
2025-09-16 17:31:45 +0300

    Created add breed page

--- 02-WebApp-CatShelter/README.md
+++ 02-WebApp-CatShelter/README.md
@@ -3,4 +3,6 @@
 -    [x] Initialize a new project
 -    [x] Create a server
 -    [x] Basic server routing
--    [x] Read read Home page from file system
+-    [x] Read Home page from file system
+-    [ ] Add create breed page
+-    [ ] Add create cat page
--- 02-WebApp-CatShelter/src/index.js
+++ 02-WebApp-CatShelter/src/index.js
@@ -2,23 +2,44 @@ import http from 'http';
 import fs from 'fs/promises';
 
 const server = http.createServer(async (req, res) => {
-	if (req.url === '/') {
-		const homeHtml = await fs.readFile('./src/views/index.html', {
-			encoding: 'utf-8',
-		});
+	let html;
 
-		res.writeHead(200, { 'content-type': 'text/html' });
-		res.write(homeHtml);
-	} else if (req.url === '/styles/site.css') {
-		const css = await fs.readFile('./src/styles/site.css');
+	switch (req.url) {
+		case '/':
+			html = await homeView();
+			break;
 
-		res.writeHead(200, { 'content-type': 'text/css' });
-		res.write(css);
+		case '/cats/add-breed':
+			html = await addBreedView();
+			break;
+
+		case '/styles/site.css':
+			const cssSite = await fs.readFile('./src/styles/site.css');
+			res.writeHead(200, { 'content-type': 'text/css' });
+			res.write(cssSite);
+			return res.end();
+		default:
+			return res.end();
 	}
 
+	res.writeHead(200, { 'content-type': 'text/html' });
+	res.write(html);
 	res.end();
 });
 
 server.listen(5000);
 
 console.log('Server running on http://localhost:5000...');
+
+async function homeView() {
+	const html = await fs.readFile('./src/views/index.html', {
+		encoding: 'utf-8',
+	});
+	return html;
+}
+async function addBreedView() {
+	const html = await fs.readFile('./src/views/addBreed.html', {
+		encoding: 'utf-8',
+	});
+	return html;
+}
